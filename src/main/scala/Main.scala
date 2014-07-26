c29cc464cf69ac20f808cb7d21782e83861171c1
Aleksey Izmailov <izmailoff@gmail.com>
2014-07-26 18:34:09 +0800

    Initial check-in

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,22 @@
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+ 
+class HelloActor extends Actor {
+  def receive = {
+    case "hello" => println("hello back at you")
+    case _       => println("huh?")
+  }
+}
+ 
+object Main extends App {
+  val system = ActorSystem("HelloSystem")
+  // default Actor constructor
+  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "buenos dias"
+
+  Thread.sleep(5000)
+  system.shutdown()
+}
+
